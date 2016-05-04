0b724ac078327894c38ea894acdbaf04acf87eef
Martin Milev <martinmariusmilev@gmail.com>
2016-05-04 17:18:32 +0300

    adding all collections task with fixes from previous pull request

---
+++ src/com/clouway/collections/pagebean/PreviousPageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.clouway.collections.pagebean;
+
+public class PreviousPageNotFoundException extends Exception {
+  public PreviousPageNotFoundException() {
+    super("\nYou are on the first page.");
+  }
+}
