2b527eb6f09109f5f637725ed94c53bfb34756d1
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-06-10 16:06:22 +0300

    Done with PageBean and Sentences

--- src/secondtask/Sentence.java
+++ src/secondtask/Sentence.java
@@ -11,10 +11,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by clouway on 03.06.16.
  */
 public class Sentence {
-    private Map<Character, Integer> map = new HashMap<>();
+
     Map.Entry<Character,Integer> entry;
 
     public Map<Character, Integer> getCharFreq(String s) {
+         Map<Character, Integer> map = new HashMap<>();
         char[] chararray = s.toCharArray();
         for (char i : chararray) {
             map.put(i, map.get(i) == null ? 1 : map.get(i) + 1);
@@ -24,6 +25,7 @@ public class Sentence {
     }
 
     public Character mostFrequent(String s) {
+        Map<Character, Integer> map = new HashMap<>();
         int maxValueInTheMap = (Collections.max(map.values()));
         for (Map.Entry<Character,Integer> entry : map.entrySet())
             if (entry.getValue() == maxValueInTheMap) {
