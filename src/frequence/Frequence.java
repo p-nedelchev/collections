f7e75e2d822443bd8304a1b47e8ece89b0c3ef8f
Denis Dimitrov <denis.k.dimitrov@gmail.com>
2016-07-13 14:04:07 +0300

    Frequence task from Collections

---
+++ src/frequence/Frequence.java
@@ -0,0 +1,45 @@
+package frequence;
+
+import java.util.*;
+
+/**
+ * Created by clouway on 11.07.16.
+ *
+ * @author Denis Dimitrov(@denis.k.dimitrov@gmail.com)
+ */
+public class Frequence {
+    private final String text;
+
+    public Frequence(String text) {
+        this.text = text;
+    }
+
+    public Map<Character, Integer> charFrequence() {
+        Map<Character, Integer> result = new LinkedHashMap<>();
+        for (Character each : text.toCharArray()) {
+            result.put(each, result.get(each) == null ? 1 : result.get(each) + 1);
+        }
+        return result;
+    }
+
+    public Character mostFrequentCharacter() {
+        Map<Character, Integer> freqChar = charFrequence();
+        Integer maxValue = Collections.max(freqChar.values());
+        for (Character each : freqChar.keySet()) {
+            if (freqChar.get(each).equals(maxValue)) {
+                return each;
+            }
+        }
+        Optional<Integer> possible = Optional.of(maxValue);
+        possible.isPresent();
+        return null;
+    }
+
+    public Map<String, Integer> wordFrequence(String[] text) {
+        Map<String, Integer> result = new LinkedHashMap<>();
+        for (String each : text) {
+            result.put(each, result.get(each) == null ? 1 : result.get(each) + 1);
+        }
+        return result;
+    }
+}
