be0e96f25ce5d9584bed863de82114b9a8d73416
Denis Dimitrov <denis.k.dimitrov@gmail.com>
2016-07-15 09:46:52 +0300

    Better readability.

--- src/frequence/Frequence.java
+++ src/frequence/Frequence.java
@@ -14,14 +14,6 @@ public class Frequence {
         this.text = text;
     }
 
-    public Map<Character, Integer> charFrequence() {
-        Map<Character, Integer> result = new LinkedHashMap<>();
-        for (Character each : text.toCharArray()) {
-            result.put(each, result.get(each) == null ? 1 : result.get(each) + 1);
-        }
-        return result;
-    }
-
     public Character mostFrequentCharacter() {
         Map<Character, Integer> freqChar = charFrequence();
         Integer maxValue = Collections.max(freqChar.values());
@@ -30,8 +22,6 @@ public class Frequence {
                 return each;
             }
         }
-        Optional<Integer> possible = Optional.of(maxValue);
-        possible.isPresent();
         return null;
     }
 
@@ -39,6 +29,14 @@ public class Frequence {
         String[] split = text.split(" ");
         Map<String, Integer> result = new LinkedHashMap<>();
         for (String each : split) {
+            result.put(each, result.get(each) == null ? 1 : result.get(each) + 1);
+        }
+        return result;
+    }
+
+    private Map<Character, Integer> charFrequence() {
+        Map<Character, Integer> result = new LinkedHashMap<>();
+        for (Character each : text.toCharArray()) {
             result.put(each, result.get(each) == null ? 1 : result.get(each) + 1);
         }
         return result;
